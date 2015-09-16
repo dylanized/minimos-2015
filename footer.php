80dc119270f375563006f7e82e1a0a0e84947e5d
Dylan Hassinger <dylanhassinger@gmail.com>
2015-09-15 23:16:28 -0500

    initial commit

--- footer.php
+++ footer.php
@@ -1,4 +1,14 @@
 
+	<footer class="row footer">
+	
+		<div class="container">
+	
+			<p><small><a href="http://twitter.com/dylanized">by @dylanized</a></small></p>
+			
+		</div>
+	
+	</footer>
+
 	<!-- wp footer -->	
 	<?php wp_footer(); ?>
 
