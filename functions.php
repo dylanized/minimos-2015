f67b803fa2295de67d44be7723e5ceda82e4cd8d
Dylan Hassinger <dylanhassinger@gmail.com>
2015-09-19 04:41:06 -0500

    added skins.css

--- functions.php
+++ functions.php
@@ -10,6 +10,7 @@
 		
 		// theme css
 		wp_enqueue_style("theme-core", get_template_directory_uri() . "/css/core.css");			
+		wp_enqueue_style("theme-core", get_template_directory_uri() . "/css/skins.css");			
 		wp_enqueue_style("theme-utility", get_template_directory_uri() . "/css/utility.css");
 		wp_enqueue_style("theme-colors", get_template_directory_uri() . "/css/colors.css");			
 		wp_enqueue_style("theme-media", get_template_directory_uri() . "/css/media.css");			
