35631c8fafd4ac3d1fb7504d895edb19a2383392
Dylan Hassinger <dylanhassinger@gmail.com>
2015-09-19 20:12:48 -0500

    adding hovers

--- footer.php
+++ footer.php
@@ -3,8 +3,8 @@
 	<div class="container">
 
 		<p class="float-right text-right font-25">
-			<a href="http://twitter.com/minimosWP"><i class="fa fa-twitter"></i></a>&nbsp;
-			<a href="http://github.com/dylanized/minimos-wp"><i class="fa fa-github"></i></a>
+			<a class="hover-blue" href="http://twitter.com/minimosWP"><i class="fa fa-twitter"></i></a>&nbsp;
+			<a class="hover-green" href="http://github.com/dylanized/minimos-wp"><i class="fa fa-github"></i></a>
 		</p>
 
 		<p><a href="http://minimos.net">built with Minimos</a></p>
