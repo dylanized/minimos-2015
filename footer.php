4c5ff4046ff4692f3d21152476d03c5d8e6e618d
Dylan Hassinger <dylanhassinger@gmail.com>
2015-09-19 20:02:03 -0500

    removed importants

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 
 	<div class="container">
 
-		<p class="float-right text-right font-20">
+		<p class="float-right text-right font-25">
 			<a href="http://twitter.com/minimosWP"><i class="fa fa-twitter"></i></a>&nbsp;
 			<a href="http://github.com/dylanized/minimos-wp"><i class="fa fa-github"></i></a>
 		</p>
