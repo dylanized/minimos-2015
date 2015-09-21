a7241b0d3fc6f6e8b59051ef36faf86b5172c95a
Dylan Hassinger <dylanhassinger@gmail.com>
2015-09-20 21:58:24 -0500

    broke out loop into separate file

--- index.php
+++ index.php
@@ -4,51 +4,7 @@
 
 	<div class="container">
 	
-		<?php if (have_posts()) : ?>
-	
-			<?php while (have_posts()) : the_post(); ?>
-	
-				<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-	
-					<header class="post-header">
-					
-						<?php if (is_singular()) { ?>
-							<h1><?php the_title(); ?></h1>
-						<?php } else { ?>
-							<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-						<?php } ?>
-						
-						<!-- meta goes here -->
-						
-					</header>
-					
-					<section class="post-content">
-					
-						<?php the_content('Read More &raquo;'); ?>
-					
-					</section>
-					
-					<footer class="post-footer">
-					
-						<!-- meta goes here -->
-					
-					</footer>
-					
-					<!-- comments go here -->
-
-				</article>
-	
-			<?php endwhile; ?>
-	
-		<?php else : ?>
-		
-			<section class="post">
-	
-				<p><em>Sorry, no posts found!</em></p>
-				
-			</section>
-	
-		<?php endif; ?>
+		<?php get_template_part("loop"); ?>
 	
 	</div>
 
---
+++ loop.php
@@ -0,0 +1,49 @@
+<?php if (have_posts()) : ?>
+	
+	<?php while (have_posts()) : the_post(); ?>
+
+		<article <?php post_class("margin-bottom-60") ?> id="post-<?php the_ID(); ?>">
+
+			<header class="post-header">
+			
+				<?php if (is_singular()) { ?>
+					<h1 class="margin-10"><?php the_title(); ?></h1>
+				<?php } else { ?>
+					<h1 class="margin-10"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+				<?php } ?>
+				
+				<!-- meta goes here -->
+				
+			</header>
+			
+			<section class="post-content">
+			
+				<?php the_content('Read More &raquo;'); ?>
+			
+			</section>
+			
+			<?php if (!is_page()) { ?>
+			
+				<footer class="post-footer margin-top--20 color-lite">
+				
+					<p><small><?php the_time('F jS, Y') ?> | <?php the_category(', ') ?></small></p>
+				
+				</footer>
+				
+			<?php } ?>
+			
+			<!-- comments go here -->
+
+		</article>
+
+	<?php endwhile; ?>
+
+<?php else : ?>
+
+	<section class="post">
+
+		<p><em>Sorry, no posts found!</em></p>
+		
+	</section>
+
+<?php endif; ?>
