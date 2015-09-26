63b5fad9f0df42d6759bbe6ac695ed467b4f1a65
Dylan Hassinger <dylanhassinger@gmail.com>
2015-09-25 22:05:54 -0500

    changed order of assets

--- functions.php
+++ functions.php
@@ -11,12 +11,16 @@
 		// libraries
 		wp_enqueue_style("minimos-normalize", get_template_directory_uri() . "/lib/css/normalize.css");
 		wp_enqueue_style("minimos-grid", get_template_directory_uri() . "/lib/css/simplegrid.css");			
-		wp_enqueue_style("minimos-utility", get_template_directory_uri() . "/css/utility.css");
-		wp_enqueue_style("minimos-colors", get_template_directory_uri() . "/css/colors.css");			
 		
 		// theme css
 		wp_enqueue_style("theme-core", get_template_directory_uri() . "/css/core.css");			
-		wp_enqueue_style("theme-skins", get_template_directory_uri() . "/css/skins.css");			
+		wp_enqueue_style("theme-skins", get_template_directory_uri() . "/css/skins.css");
+		
+		// utility css
+		wp_enqueue_style("minimos-utility", get_template_directory_uri() . "/css/utility.css");
+		wp_enqueue_style("minimos-colors", get_template_directory_uri() . "/css/colors.css");			
+				
+		// mobile devices		
 		wp_enqueue_style("theme-media", get_template_directory_uri() . "/css/media.css");			
 		
 		// site overrides
